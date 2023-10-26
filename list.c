6a402fecdfa64690dc962c05f846c68d83d4092e
Lizab48 <129211040+Lizab48@users.noreply.github.com>
2023-10-26 12:23:45 +0200

    fct create_list

--- list.c
+++ list.c
@@ -4,3 +4,11 @@
 
 #include "list.h"
 
+#include <stdlib.h>
+
+p_sk_list Create_list (int level_max){
+    p_sk_list p_list;
+    p_list->max_level = level_max;
+    p_list->head = (p_sk_list*)malloc(sizeof(p_sk_list)*level_max);
+    return p_list;
+}
